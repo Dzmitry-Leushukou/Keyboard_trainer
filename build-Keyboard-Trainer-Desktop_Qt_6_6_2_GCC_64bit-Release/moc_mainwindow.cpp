a52491fda458253c339050f237f096927748d9fb
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-05-03 10:42:30 +0300

    Release version

---
+++ build-Keyboard-Trainer-Desktop_Qt_6_6_2_GCC_64bit-Release/moc_mainwindow.cpp
@@ -0,0 +1,220 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.6.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../Task_1/mainwindow.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.6.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "keyReleaseEvent",
+    "",
+    "QKeyEvent*",
+    "ke",
+    "keyPressEvent",
+    "on_comboBox_currentIndexChanged",
+    "index",
+    "on_checkBox_stateChanged",
+    "arg1",
+    "on_StartButton_clicked",
+    "update_gui",
+    "on_OpenFileButton_clicked"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
+    uint offsetsAndSizes[26];
+    char stringdata0[11];
+    char stringdata1[16];
+    char stringdata2[1];
+    char stringdata3[11];
+    char stringdata4[3];
+    char stringdata5[14];
+    char stringdata6[32];
+    char stringdata7[6];
+    char stringdata8[25];
+    char stringdata9[5];
+    char stringdata10[23];
+    char stringdata11[11];
+    char stringdata12[26];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 15),  // "keyReleaseEvent"
+        QT_MOC_LITERAL(27, 0),  // ""
+        QT_MOC_LITERAL(28, 10),  // "QKeyEvent*"
+        QT_MOC_LITERAL(39, 2),  // "ke"
+        QT_MOC_LITERAL(42, 13),  // "keyPressEvent"
+        QT_MOC_LITERAL(56, 31),  // "on_comboBox_currentIndexChanged"
+        QT_MOC_LITERAL(88, 5),  // "index"
+        QT_MOC_LITERAL(94, 24),  // "on_checkBox_stateChanged"
+        QT_MOC_LITERAL(119, 4),  // "arg1"
+        QT_MOC_LITERAL(124, 22),  // "on_StartButton_clicked"
+        QT_MOC_LITERAL(147, 10),  // "update_gui"
+        QT_MOC_LITERAL(158, 25)   // "on_OpenFileButton_clicked"
+    },
+    "MainWindow",
+    "keyReleaseEvent",
+    "",
+    "QKeyEvent*",
+    "ke",
+    "keyPressEvent",
+    "on_comboBox_currentIndexChanged",
+    "index",
+    "on_checkBox_stateChanged",
+    "arg1",
+    "on_StartButton_clicked",
+    "update_gui",
+    "on_OpenFileButton_clicked"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,   56,    2, 0x08,    1 /* Private */,
+       5,    1,   59,    2, 0x08,    3 /* Private */,
+       6,    1,   62,    2, 0x08,    5 /* Private */,
+       8,    1,   65,    2, 0x08,    7 /* Private */,
+      10,    0,   68,    2, 0x08,    9 /* Private */,
+      11,    0,   69,    2, 0x08,   10 /* Private */,
+      12,    0,   70,    2, 0x08,   11 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, QMetaType::Int,    7,
+    QMetaType::Void, QMetaType::Int,    9,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'keyReleaseEvent'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QKeyEvent *, std::false_type>,
+        // method 'keyPressEvent'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QKeyEvent *, std::false_type>,
+        // method 'on_comboBox_currentIndexChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_checkBox_stateChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'on_StartButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'update_gui'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_OpenFileButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->keyReleaseEvent((*reinterpret_cast< std::add_pointer_t<QKeyEvent*>>(_a[1]))); break;
+        case 1: _t->keyPressEvent((*reinterpret_cast< std::add_pointer_t<QKeyEvent*>>(_a[1]))); break;
+        case 2: _t->on_comboBox_currentIndexChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 3: _t->on_checkBox_stateChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 4: _t->on_StartButton_clicked(); break;
+        case 5: _t->update_gui(); break;
+        case 6: _t->on_OpenFileButton_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 7;
+    }
+    return _id;
+}
+QT_WARNING_POP
